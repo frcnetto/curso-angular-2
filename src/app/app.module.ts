813cfe3b372927e9c3e05fa6f579347e101c3537
Netto Cavalcante <frcnetto@gmail.com>
2017-03-11 19:37:53 -0300

    Aula components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from  './app.component';
 import { MeuPrimeiroComponent} from './meu-primeiro/meu-primeiro.component';
 import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
+import { CursosModule } from './cursos/cursos.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MeuSegundoComponent } from './meu-segundo/meu-segundo.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
